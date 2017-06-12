8323e394a2fffbfef4250e9c4b6f85676353095e
H.T. Koo <h1017760@connect.hku.hk>
2017-06-12 15:11:41 +0800

    FreeCodeCamp - Back End Development Certification - API projects: Request_Header_Parser_Microservice - initial project setup

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var timestampParser = require("../service/timestampParser");
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', {});
+});
+
+router.get('/:timestamp', function (req, res) {
+    var timestamp = req.params.timestamp;
+    res.send(timestampParser(timestamp));
+});
+
+module.exports = router;
